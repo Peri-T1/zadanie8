4396f1ce1e5112033933829d17ec7de15065b34c
ваше имя <email@example.com>
2024-03-29 09:48:02 +0300

    zadanie8

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+var color = prompt('Введите цвет цветафора')
+if (color == 'красный') {
+  alert('стой')
+} else if (color == 'жёлтый') {
+    alert('жди')
+} else if (color =='зелёный') {
+    alert('иди')
+} else {
+    alert('в цветафоре нет такого цвета')
+}
